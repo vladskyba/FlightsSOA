b2ebfad0325c6c9debf5ec3c015d0093076a45a2
Vladyslav Skyba <101654443+vladskyba@users.noreply.github.com>
2022-12-24 21:37:22 +0200

    Swagger configure

--- ServiceRegistry/Startup.cs
+++ ServiceRegistry/Startup.cs
@@ -64,10 +64,10 @@ namespace ServicesRegistry
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ServicesRegistry v1"));
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ServicesRegistry v1"));
             app.UseHttpsRedirection();
 
             app.UseRouting();
--- Services/AirplaneService/Startup.cs
+++ Services/AirplaneService/Startup.cs
@@ -28,7 +28,6 @@ namespace Airplane
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -59,7 +58,6 @@ namespace Airplane
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
